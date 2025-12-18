1e304805c0610f2dbb2c2de3e1ae9ea2a71f027c
Noam2Stein <noamsteindev@gmail.com>
2025-12-18 08:05:21 +0200

    Add Email newtype

---
+++ shared/email.py
@@ -0,0 +1,19 @@
+import re
+
+class Email:
+    _EMAIL_REGEX = re.compile(r"^[a-zA-Z0-9_+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-]+$")
+
+    def __init__(self, string: str):
+        if not self._EMAIL_REGEX.match(string):
+            raise RuntimeError(f"Invalid email: {string}")
+        
+        self._string = string
+
+    def __eq__(self, other):
+        return self._string == other._string
+    
+    def __repr__(self):
+        return f"Email(\"{self._string}\")"
+
+    def string(self) -> str:
+        return self._string
