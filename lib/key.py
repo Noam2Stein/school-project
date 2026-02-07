e462cbbe4c2fca1b7fffc7e3944ed6ab87fa74d6
Noam2Stein <noamsteindev@gmail.com>
2026-02-07 18:52:29 +0200

    Replace hashing dependency `argon2` with `hashlib`
    `argon2` requires compiling some C or C++ code and so requires some build tool.
    My school decided to block installing things on the computers!... hopefully this
    dependency will work.

--- lib/key.py
+++ lib/key.py
@@ -1,4 +1,4 @@
-import argon2
+import hashlib
 
 class Key:
     """
@@ -22,14 +22,13 @@ class Key:
     
     def hash(self) -> 'Key':
         key_bytes = self.value.to_bytes(32)
-        hash_bytes = argon2.low_level.hash_secret_raw(
-            secret=key_bytes,
-            salt=b"yarden-cohen",
-            time_cost=3,
-            memory_cost=102400,
-            parallelism=4,
-            hash_len=32,
-            type=argon2.low_level.Type.ID,
+        hash_bytes = hashlib.scrypt(
+            password=key_bytes,
+            salt="yarden-cohen",
+            # `n` is internal memory size. (python has no docs...).
+            n=2**15,
+            # output byte count. (python has no docs...).
+            dklen=32,
         )
 
         return Key(int.from_bytes(hash_bytes))
